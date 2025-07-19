348d1ae5d508067440e161bab3349a336421c277
whitening Glow <whiteningglow99@gmail.com>
2025-07-19 12:12:39 +0000

    Audit lagi yang berkaitan dengan npm run build apakah ada lagi error, pe

---
+++ src/next.config.ts
@@ -0,0 +1,22 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      }
+    ],
+     // Allow data URIs
+    dangerouslyAllowSVG: true,
+  },
+};
+
+export default nextConfig;
